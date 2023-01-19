24700c3689ba18c80bd340050f5069b77d6419e7
Gabriela Alvarez <galva041@ucr.edu>
2023-01-18 23:14:46 -0800

    styled bg and more buttons

--- StudyMates/screens/landing.js
+++ StudyMates/screens/landing.js
@@ -1,28 +1,59 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image, ImageBackground} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import customButton from "../customButton";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Landing = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+    style={styles.linGrad}
+    colors={["#FFE4E4", "#FFB2B2", "#B0B0F8", "#588046", "#206003"]}
+    start={{ x: 0, y: 0 }}
+    locations={["0.77%", "37.93%", "60.5%", "65.94%", "96.15%"]}
+    >
+      {/* <ImageBackground source={require('../assets/callout.png')} resizeMode="cover" style={styles.image}>
+      </ImageBackground> */}
+      
+      {/* https://reactnative.dev/docs/images */}
+      <Image source={require('../assets/greenHyena2.png')} />
 
-      <Pressable style={styles.button} onPress={() => navigation.navigate("TimerPick")}>
-        <Text style={styles.buttonText}>Study</Text> 
+      {/* <ImageBackground source={require('../assets/calloutBubble.png')} 
+        style={styles.image}>
+          <Text>hey lol</Text>
+      </ImageBackground> */}
+
+      <Pressable style={[styles.button, styles.shadowProp]} onPress={() => navigation.navigate("TimerPick")}>
+        <Text style={styles.buttonText}>study</Text> 
       </Pressable>
 
-      {/* <customButton onPressFunction={navigation.navigate("TimerPick")}
-      title = "study"/> */}
-      {/* <Text>Landing</Text> */}
-    </View>
+      <Pressable style={[styles.roundButton, styles.shadowProp]}>
+        <MaterialCommunityIcons name="hanger" size={24} color="black" /> 
+      </Pressable>
+
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  linGrad: {
+    alignItems: 'center',
+    justifyContent: 'center',
     flex: 1,
-    backgroundColor: "#FFB2B2",
-    alignItems: "center",
-    justifyContent: "center"
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 150,
+    backgroundColor: "red",
+  },
+  shadowProp: {
+    shadowColor: '#00000',
+    shadowOffset: {height: 4},
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
   },
   button: { 
     backgroundColor: "#E6E0FF",
@@ -31,6 +62,13 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     height: 56,
     width: 181,
+  },
+  roundButton: {
+    backgroundColor: "#E6E0FF",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius:100,
+    padding: 10,
   },
   buttonText: {
     fontFamily: 'WorkSansMedium',
